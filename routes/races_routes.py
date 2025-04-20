363c0b75120d3b6e5bde61a3eb26553dd7332a89
LAPTOP-7FAJG6I2\81909 <axxxhiratani@gmial.com>
2025-04-20 11:45:57 +0900

    トピックスのリストを取得するルートを作成

--- routes/races_routes.py
+++ routes/races_routes.py
@@ -8,3 +8,10 @@ def get_races():
     # 一旦ダミーデータを返す
     # http://127.0.0.1:5000/api/v2/races
     return jsonify({"event": "ダミーレース", "date": "2025-04-20"})
+
+@races_bp.route('/api/v2/races/topic_race', methods=['GET'])
+def get_topic_race():
+    # ここにロジックを実装
+    # 一旦ダミーデータを返す
+    # http://127.0.0.1:5000/api/v2/races/topic_race
+    return jsonify({"date": "Topick test"})
