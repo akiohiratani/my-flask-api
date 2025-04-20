6fdbdc35331ae8ed6770b0c454f2370666c88837
LAPTOP-7FAJG6I2\81909 <axxxhiratani@gmial.com>
2025-04-20 21:39:36 +0900

    検証用クラスを追加

--- output/output.py
+++ output/output.py
@@ -10,3 +10,22 @@ class Output:
                 f.write(f"Table {i}:\n")
                 f.write(table.prettify())
                 f.write('\n' + '-'*40 + '\n')
+    # Div要素を全て出力するメソッド
+    def outputDiv(self, soup:BeautifulSoup):
+        # Div要素を全て取得
+        divs = soup.find_all('div')
+        with open('output/divs.txt', 'w', encoding='utf-8') as f:
+            for i, div in enumerate(divs, 1):
+                f.write(f"Div {i}:\n")
+                f.write(div.prettify())
+                f.write('\n' + '-'*40 + '\n')
+
+    # Div要素をクラス名で指定して全て出力するメソッド
+    def outputDivForClass(self, soup:BeautifulSoup, className:str):
+        # Div要素を全て取得
+        divs = soup.find_all('div', class_=className)
+        with open('output/divs.txt', 'w', encoding='utf-8') as f:
+            for i, div in enumerate(divs, 1):
+                f.write(f"Div {i}:\n")
+                f.write(div.prettify())
+                f.write('\n' + '-'*40 + '\n')
