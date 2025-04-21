f427b21dc81533d8806f9482bbb1ce4341cf1bc1
LAPTOP-7FAJG6I2\81909 <axxxhiratani@gmial.com>
2025-04-22 06:34:00 +0900

    馬の名前と基本情報を取得

--- services/horce_client.py
+++ services/horce_client.py
@@ -26,15 +26,25 @@ class HorseClient(BaseClient):
 
         # 馬の基本情報を取得
         ## ヴァルキリーバース 現役　牝3歳
+        self.get_horse_base_info(soup)
 
         # 馬の画像URL取得
         image = self.get_horse_image(soup)
 
         #馬の血統を取得
         ## 父：エピファネイア, 母父：ハーツクライ
-
-        #Output().outputTableForClass(soup, "db_prof_table ")
+        ## db_prof_box
         return
+    def get_horse_base_info(self, soup:BeautifulSoup):
+        #Output().outputDivForClass(soup, "horse_title")
+        horse_info = soup.find("div", class_="horse_title")
+
+        #名前の取得
+        name = horse_info.find("h1").text
+        info = horse_info.find("p", class_="txt_01").text
+
+        return
+
     
     def get_horse_image(self, soup:BeautifulSoup) -> str:
         main_photo = soup.find(id="HorseMainPhoto")
