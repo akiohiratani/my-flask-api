eecbd6346040c4989740cd06baf0e1323432a835
LAPTOP-7FAJG6I2\81909 <axxxhiratani@gmial.com>
2025-04-21 21:39:51 +0900

    コメント追加

--- services/horce_client.py
+++ services/horce_client.py
@@ -22,5 +22,15 @@ class HorseClient(BaseClient):
     def get_hours(self, id:str):
         url = self.BASE_URL.format(id)
         soup = self.get_soup(url)
-        Output().outputTable(soup)
+
+        # 馬の基本情報を取得
+        ## ヴァルキリーバース 現役　牝3歳
+
+        # 馬の画像取得
+        ## https://cdn.netkeiba.com/img.db/v1.1/show_photo.php?horse_id=2022104764&no=5671&tn=yes&tmp=no
+
+        #馬の血統を取得
+        ## 父：エピファネイア, 母父：ハーツクライ
+
+        Output().outputTableForClass(soup, "db_prof_table ")
         return
