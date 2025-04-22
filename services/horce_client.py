a753c79b91e324435bf02b39c11d3895423dce7d
LAPTOP-7FAJG6I2\81909 <axxxhiratani@gmial.com>
2025-04-23 06:01:19 +0900

    主な勝鞍を取得するメソッド作成

--- services/horce_client.py
+++ services/horce_client.py
@@ -31,11 +31,16 @@ class HorseClient(BaseClient):
         # 馬の画像URL取得
         image = self.get_horse_image(soup)
 
-        #馬の血統を取得
+        # 馬の血統を取得
         ## 例：父：エピファネイア, 母父：ハーツクライ
         blood = self.get_horse_blood(soup)
         
+        # 馬の主な勝鞍を取得
+        ## db_prof_table
+        title = self.get_horse_title(soup)
+
         return
+
     def get_horse_base_info(self, soup:BeautifulSoup):
         horse_info = soup.find("div", class_="horse_title")
 
@@ -64,3 +69,25 @@ class HorseClient(BaseClient):
             "father":horse_names[0],
             "grandfather":horse_names[1]
         }
+    
+    def get_horse_title(self, soup:BeautifulSoup):
+        #Output().outputTableForClass(soup, "db_prof_table")
+        horse_info = {}
+        prof_table = soup.find("table", class_="db_prof_table")
+        for tr in prof_table.find_all('tr'):
+            th = tr.find('th')
+            td = tr.find('td')
+            if th and td:
+                key = th.get_text(strip=True)
+                if key == '生年月日':
+                    horse_info['birthday'] = td.get_text(strip=True)
+                elif key == '主な勝鞍':
+                    a = td.find('a')
+                    if a:
+                        horse_info['title'] = a.get_text(strip=True)
+                elif key == '近親馬':
+                    relatives = '、'.join([a.get_text(strip=True) for a in td.find_all('a')])
+                    horse_info['Close relative'] = relatives
+                else:
+                    horse_info[key] = td.get_text(strip=True)
+        return horse_info["title"]
